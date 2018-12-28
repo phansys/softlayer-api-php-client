e353a3fbd91b4b172b4bfc6f5d95f7c1a6dacfd2
Javier Spagnoletti <phansys@gmail.com>
2018-12-28 14:04:15 -0300

    Add tests

--- src/SoapClient.php
+++ src/SoapClient.php
@@ -200,7 +200,7 @@ class SoapClient extends \SoapClient
 
         try {
             $result = parent::__call($functionName, $arguments, null, $this->_headers, null);
-        } catch (SoapFault $e) {
+        } catch (\SoapFault $e) {
             throw new \Exception('There was an error querying the SoftLayer API: '.$e->getMessage());
         }
 
